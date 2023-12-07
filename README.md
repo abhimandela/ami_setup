83221805848069e578b5b7f53620e28cf57cd413
Abhi Mandela <abhimandela@gmail.com>
2023-12-07 17:59:49 +0000

    Initial commit with necessary scripts to update configurations, new JSON config file, updated README, add requirements.txt, removed non essentail scripts for now

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # ami_setup
-AMI configuration files
+This is the branch to modify AMI configurations to be used with the WittyPi L3V7 unit with 18650 battery holder and 18650 battery. 
+
+WittyPi L3V7 https://www.uugear.com/product/witty-pi-4-l3v7/ can be used a single board solution that provides RTC, Watchdog and UPS functionalities intended for the AMI systems. This add on board to Raspberry Pi also provides capabilities to  schedule ON and OFF times with custom schedules.
+
+AMI configuration files here have been adjusted to be used only with the WittyPi L3V7 and Raspberry Pi 4B with raspbian OS image version x.y.z.q. It is not tested and not guaranteed to work with other versions.
+
+# Configuration Update Script
+
+This script reads configuration settings from a JSON file and updates various configuration files accordingly.
+
+Instructions to update AMI configurations:
+1. Edit config.json to updated with appropriate values
+2. Run the set_ami_config.py to set all necessary configurations 
+3. Paths should be absolute, and eveything should work as long as the paths and values entered are correct
+4. TODO: Include DATA validation 
+
+## Usage
+
+1. Install the required dependencies by running:
+
+   ```bash
+   pip install -r requirements.txt
+
+
+2. Set the configurations by running the set_ami_config.py as below:
+
+* The script has to be run from the directory where config.json and this file are located
+* Other files that are bring modified like the motion.conf and heliocron.toml must be present in respective paths
+* From commandline that has Python3 enabled, run the command "sudo python3 set_ami_config.py"
+* The above command prints a summary of changes done to script, make sure the changes intended are reflected correctly. 
+* The script does not validate for correct input values that can be entered atleast not at this point 
+
+This code aims to achieve the following:
+1. Requirements file for installing necessary packages for the WittyPi functionality integration 
+2. Ability to configure AMI systems with snapshot interval
+3. Ability to set different camera settings available 
+4. Ability to disable and enable motion detection 
+5. Ability to set operating times using sunset and sunrise times from a given location 
+
+The set_ami_config.py script does the following after reading the config.json file:
+1. Update location in ~/.config/helicocron.toml
+2. Update Camera ID by using the command "ls -dev/v4l/by-id/" in the following files and camera1.conf
+3. Default videodevice to be used for capturing is "/dev/video0"
+4. Update camera setttings in /home/pi/scripts/setCamera.sh 
+5. Update motion configuration in /etc/motion/motion.conf
+
+Configuration Files
+
+    heliocron.toml: The script updates the latitude and longitude in the Heliocron TOML configuration file located at ~/.config/heliocron.toml.
+
+    setCamera.sh: The script updates camera settings in the shell script located at /home/pi/scripts/setCamera.sh.
+
+    motion.conf: The motion software configuration file located at /etc/motion/motion.conf is updated with motion-related settings.
+
+    camera1.conf: The camera configuration file in the motion folder located at /etc/motion/camera1.conf is updated.
+
+Note: Ensure that you have the necessary permissions to modify the specified configuration files. Running scripts as sudo ensures that there will be no failures due to previlege settings 
+
+Dependencies
+
+The script uses the following Python libraries:
+
+    toml: A library for working with TOML configuration files.
+
+Disclaimer
+
+Make sure to back up your original configuration files before running this script to avoid any unintended changes.
---
+++ set_ami_config.py
@@ -0,0 +1,190 @@
+#Script to set configiration set in config.json 
+import json
+import toml
+import re
+import subprocess
+
+# Load the config paramters from the JSON file
+def read_json_config(json_path):
+    with open(json_path, 'r') as json_file:
+        return json.load(json_file)
+
+# Update the Heliocron settings under the recommended config file 
+def update_toml_config(toml_path, location_data):
+    with open(toml_path, 'r') as toml_file:
+        config = toml.load(toml_file)
+        #print ("toml PATH: ", toml_path," Config: ",config)
+
+    # Update location information
+    #print ("Lat: ",location_data['location']['lat'])
+    #print ("Lon: ",location_data['location']['lon'])
+    config['latitude'] = location_data['location']['lat']
+    config['longitude'] = location_data['location']['lon']
+
+    # Write the updated TOML configuration back to the file
+    with open(toml_path, 'w') as toml_file:
+        toml.dump(config, toml_file)
+
+# Run the command "ls -l /dev/v4l/by-id/" and get the first line
+def get_camera_id():
+    try:
+        ls_output = subprocess.check_output(["ls", "/dev/v4l/by-id/"], text=True)
+        camera_id = ls_output.split('\n')[0].split()[-1]
+        print ("camera ID is: ",camera_id)
+        return camera_id
+    except subprocess.CalledProcessError as e:
+        print(f"Error running 'ls' command: {e}, defaulting to /dev/video0")
+        camera_id = '/dev/video0'
+        return None
+
+# Configure 'motion' software related paramters 
+def update_motion_config(script_path, motion_data):
+    with open(script_path, 'r') as script_file:
+        script_lines = script_file.readlines()
+    
+    # Get the camera ID
+    camera_id = get_camera_id()
+
+    #print (motion_data['motion'].items())
+
+    # Update motion settings
+    for key, value in motion_data['motion'].items():
+        for i, line in enumerate(script_lines):
+            
+            # Ignore lines starting with #
+            if line.strip().startswith('#'):
+                continue
+
+            if line.strip() == 'videodevice':                
+                print(f"videodevice line: {line.strip()}")
+                # TODO: Update videodevice with camera ID as in other functions
+            #else:
+            #    new_line = line.replace(f"{key} {original_value}", f"{key} {value}")
+            #    print(f"Updated line: {new_line.strip()}")
+
+                
+            # Using a regular expression to find lines containing the setting key and value
+            pattern = fr'({key} )(\S+)'
+            #print ("pattern: ",pattern)
+            match = re.search(pattern, line)
+            if match:
+                original_value = match.group(2)
+                print(f"Original line: {line.strip()}")
+                # Replace the setting value while preserving the rest of the line
+                new_line = line.replace(f"{key} {original_value}", f"{key} {value}")
+                print(f"Updated line: {new_line.strip()}")
+                script_lines[i] = new_line
+                break
+
+    # Write the updated script content back to the file
+    with open(script_path, 'w') as script_file:
+        script_file.writelines(script_lines)
+
+# Update the camera config files with videodevice ID and other info
+def update_camera_config(config_path, camera_id):
+    # Read the content of the config file
+    with open(config_path, 'r') as config_file:
+        config_lines = config_file.readlines()
+
+    print ("config_path", config_path)
+    print ("camera_id", camera_id)
+
+    # Update the line containing "videodevice"
+    for i, line in enumerate(config_lines):
+        
+        # Skip lines #, usually commented out lines
+        if line.startswith('#'):
+                continue
+        
+        if 'videodevice' in line:
+                      
+            # Split the line into parts separated by spaces
+            parts = line.split()
+
+            # Replace the part after "videodevice" with the camera_id value
+            if len(parts) > 1:
+                #print ("Length ", len(parts))
+                #print ("Part[0] ",parts[0])
+                #print ("Part[1] ",parts[1])
+                parts[1] = camera_id
+
+            # Join the parts back into a line
+            new_line = ' '.join(parts)
+            print ("new_line", new_line)
+
+            # Update the list with the new line and add new line character to accommodate for join
+            config_lines[i] = new_line + '\n'
+
+            break  # Stop searching once the line is updated
+
+    # Write the updated content back to the config file
+    with open(config_path, 'w') as config_file:
+        config_file.writelines(config_lines)
+
+# Update camera settings along with the video device ID / Camera ID 
+def update_camera_settings(script_path, camera_data):
+    with open(script_path, 'r') as script_file:
+        script_lines = script_file.readlines()
+
+    # Get the camera ID
+    camera_id = get_camera_id()
+    
+    # Update camera settings
+    for key, value in camera_data['camera'].items():
+        for i, line in enumerate(script_lines):
+            
+            # Ignore lines starting with #
+            if line.strip().startswith('#'):
+                continue
+            
+            # Using a regular expression to find lines containing the setting key and value
+            #pattern = fr'({key}=)(\S+)' # Only to replace the key and value 
+            pattern = fr'(v4l2-ctl -d \S+ -c {key}=)(\S+)'
+            match = re.search(pattern, line)
+
+            if match:
+                original_value = match.group(2)
+                print(f"Original line: {line.strip()}")
+                # Replace the setting value while preserving the rest of the line
+                #new_line = line.replace(f"{key}={original_value}", f"{key}={value}")
+                line = re.sub(fr'-d /dev/\S+', f'-d {camera_id}', line)
+                line = line.replace(f"{key}={original_value}", f"{key}={value}")
+                print(f"Updated line: {line.strip()}")
+                script_lines[i] = line
+                break
+
+    # Write the updated script content back to the file
+    with open(script_path, 'w') as script_file:
+        script_file.writelines(script_lines)
+
+if __name__ == "__main__":
+    # Replace 'config.json' with the actual path to your JSON configuration file
+    json_config_path = 'config.json'
+
+    #Initialize camera ID if camera ID lookup fails 
+    camera_id = '/dev/video0'
+
+    # Heliocron settings script is located here: '~/.config/helicocron.toml' 
+    toml_config_path = '/home/abhi/.config/heliocron.toml'
+
+    # Replace with the actual path to your shell script file as needed
+    camera_script_path = '/home/pi/scripts/setCamera.sh'
+    motion_script_path = '/etc/motion/motion.conf'
+    camera_config_path = '/etc/motion/camera1.conf'
+            
+    # Read the JSON configuration file
+    json_config = read_json_config(json_config_path)
+
+    # Update location information in the TOML configuration file
+    update_toml_config(toml_config_path, json_config)
+
+    # Update camera settings in the shell script file
+    update_camera_settings(camera_script_path, json_config)
+
+    # Update motion settings in the shell script file
+    update_motion_config(motion_script_path, json_config)
+
+    #Update camera config files in the motion folder
+    update_camera_config(camera_config_path, camera_id)
+
+    print("Configuration files updated successfully.")
